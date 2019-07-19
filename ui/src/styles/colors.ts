ada2c44775b8c97aec03838bdeaae0cb8bcdd923
brennowilliam <brennowilliam@gmail.com>
2019-07-19 18:55:27 -0300

    adds the color pallets to be used throughout the application

---
+++ ui/src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const MM_BLACK = "#231F20";
+export const DARK_BLUE = "#13276C";
+export const DARK_BLUE_2 = "#1F419B";
+export const LIGHT_BLUE = "#029DE0";
+export const LIGHT_BLUE_2 = "#92D7E7";
+export const LIGHT_GREY = "#F5F5F5";
+export const MID_GREY = "#B3B3B3";
